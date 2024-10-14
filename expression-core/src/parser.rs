8f4fa6fd1005fc27076a161cc5468a9bbc206fa1
Toon Willems <m@toonwillems.be>
2024-10-14 11:43:21 +0200

    Add ParseResult<T> type and make parsing return an actual error type instead of panic'ing

--- expression-core/src/parser.rs
+++ expression-core/src/parser.rs
@@ -1,6 +1,6 @@
 use bigdecimal::BigDecimal;
-use pest::iterators::Pairs;
-use pest::pratt_parser::PrattParser;
+use pest::{iterators::Pairs, pratt_parser::PrattParser};
+
 use pest::Parser;
 use thiserror::Error;
 
@@ -10,17 +10,15 @@ pub struct ExpressionParser;
 
 impl ExpressionParser {
     #[allow(clippy::result_large_err)]
-    pub fn parse_expression(input: &str) -> ParseResult {
+    pub fn parse_expression(input: &str) -> ParseResult<Expression> {
         let mut pairs = Self::parse(Rule::root, input)?;
 
         let inner = pairs.next().unwrap().into_inner();
-        let expr = parse_expr(inner);
-        Ok(expr)
+        parse_expr(inner)
     }
 }
 
-pub type ParseResult = Result<Expression, ParseError>;
-use pest::pratt_parser::{Assoc::*, Op};
+pub type ParseResult<T> = Result<T, ParseError>;
 
 #[derive(Debug, PartialEq)]
 pub enum Function {
@@ -54,6 +52,12 @@ pub enum Expression {
 pub enum ParseError {
     #[error("Parsing error: {0}")]
     FailedToParse(#[from] ::pest::error::Error<Rule>),
+
+    #[error("Wrong number of arguments to function {0}, expected: {1}, provided: {2}")]
+    WrongNumberOfArguments(String, String, usize),
+
+    #[error("bigdecimal parsing error: {0}")]
+    FailedToParseBigDecimal(#[from] ::bigdecimal::ParseBigDecimalError),
 }
 
 #[derive(Debug, PartialEq)]
@@ -64,17 +68,41 @@ pub enum Operation {
     Divide,
 }
 
-fn parse_function(pairs: Pairs<Rule>) -> Function {
+fn parse_function(pairs: Pairs<Rule>) -> ParseResult<Function> {
     let mut iter = pairs.into_iter();
     let name = iter.next().unwrap();
-    match name.as_rule() {
-        Rule::ceil => Function::Ceil(Box::new(parse_expr(iter))),
-        Rule::concat => Function::Concat(
-            iter.map(|r| parse_expr(r.into_inner()))
-                .collect::<Vec<Expression>>(),
-        ),
+    let function = match name.as_rule() {
+        Rule::ceil => Function::Ceil(Box::new(parse_expr(iter)?)),
+        Rule::concat => {
+            let args = iter
+                .map(|r| parse_expr(r.into_inner()))
+                .collect::<ParseResult<Vec<Expression>>>()?;
+
+            Function::Concat(args)
+        }
+        Rule::round => {
+            let mut args = iter
+                .map(|r| {
+                    let expr = parse_expr(r.into_inner())?;
+                    Ok(Box::new(expr))
+                })
+                .collect::<Vec<ParseResult<Box<Expression>>>>();
+
+            match args.len() {
+                1 => Function::Round(args.remove(0)?, None),
+                2 => Function::Round(args.remove(0)?, Some(args.remove(0)?)),
+                n => {
+                    return Err(ParseError::WrongNumberOfArguments(
+                        "round".to_owned(),
+                        "1..2".to_owned(),
+                        n,
+                    ))
+                }
+            }
+        }
         rule => unreachable!("Expected function name, got :{:?}", rule),
-    }
+    };
+    Ok(function)
 }
 
 fn parse_event_attribute(mut pairs: Pairs<Rule>) -> EventAttribute {
@@ -89,21 +117,34 @@ fn parse_event_attribute(mut pairs: Pairs<Rule>) -> EventAttribute {
     }
 }
 
-fn parse_expr(pairs: Pairs<Rule>) -> Expression {
-    PrattParser::new()
-        // Addition and subtract have equal precedence
-        .op(Op::infix(Rule::add, Left) | Op::infix(Rule::subtract, Left))
-        .op(Op::infix(Rule::multiply, Left) | Op::infix(Rule::divide, Left))
-        .op(Op::prefix(Rule::unary_minus))
-        .map_primary(|primary| match primary.as_rule() {
-            Rule::function => Expression::Function(parse_function(primary.into_inner())),
-            Rule::decimal => Expression::Decimal(primary.as_str().parse().unwrap()),
-            Rule::expr => parse_expr(primary.into_inner()),
-            Rule::variable => {
-                Expression::EventAttribute(parse_event_attribute(primary.into_inner()))
-            }
-            Rule::string => Expression::String(primary.into_inner().as_str().to_owned()),
-            rule => unreachable!("Expr::parse expected atom, found {:?}", rule),
+lazy_static::lazy_static! {
+    static ref PRATT_PARSER: PrattParser<Rule> = {
+        use pest::pratt_parser::{Assoc::*, Op};
+        use Rule::*;
+
+        // Precedence is defined lowest to highest
+        PrattParser::new()
+            // Addition and subtract have equal precedence
+            .op(Op::infix(add, Left) | Op::infix(subtract, Left))
+            .op(Op::infix(multiply, Left) | Op::infix(divide, Left))
+            .op(Op::prefix(unary_minus))
+    };
+}
+
+fn parse_expr(pairs: Pairs<Rule>) -> ParseResult<Expression> {
+    PRATT_PARSER
+        .map_primary(|primary| {
+            let value = match primary.as_rule() {
+                Rule::function => Expression::Function(parse_function(primary.into_inner())?),
+                Rule::decimal => Expression::Decimal(primary.as_str().parse()?),
+                Rule::expr => parse_expr(primary.into_inner())?,
+                Rule::variable => {
+                    Expression::EventAttribute(parse_event_attribute(primary.into_inner()))
+                }
+                Rule::string => Expression::String(primary.into_inner().as_str().to_owned()),
+                rule => unreachable!("Expr::parse expected atom, found {:?}", rule),
+            };
+            Ok(value)
         })
         .map_infix(|lhs, op, rhs| {
             let op = match op.as_rule() {
@@ -113,14 +154,14 @@ fn parse_expr(pairs: Pairs<Rule>) -> Expression {
                 Rule::divide => Operation::Divide,
                 rule => unreachable!("Expr::parse expected infix operation, found {:?}", rule),
             };
-            Expression::BinOp {
-                lhs: Box::new(lhs),
+            Ok(Expression::BinOp {
+                lhs: Box::new(lhs?),
                 op,
-                rhs: Box::new(rhs),
-            }
+                rhs: Box::new(rhs?),
+            })
         })
         .map_prefix(|op, rhs| match op.as_rule() {
-            Rule::unary_minus => Expression::UnaryMinus(Box::new(rhs)),
+            Rule::unary_minus => Ok(Expression::UnaryMinus(Box::new(rhs?))),
             rule => unreachable!("Expr::parse expected operation, found {:?}", rule),
         })
         .parse(pairs)
@@ -130,81 +171,78 @@ fn parse_expr(pairs: Pairs<Rule>) -> Expression {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_parse_expression() {
-        let input = "1";
+    fn parse_and_compare(input: &str, expected_expr: Expression) {
         match ExpressionParser::parse_expression(input) {
             Ok(expr) => {
-                assert_eq!(expr, Expression::Decimal(1.into()))
+                assert_eq!(expr, expected_expr)
             }
             Err(e) => panic!("Failed to parse expression: {:?}", e),
         }
+    }
+
+    #[test]
+    fn test_parse_expression() {
+        parse_and_compare("1", Expression::Decimal(1.into()));
     }
 
     #[test]
     fn test_parse_event_attribute() {
-        let input = "event.timestamp";
-        match ExpressionParser::parse_expression(input) {
-            Ok(expr) => {
-                assert_eq!(expr, Expression::EventAttribute(EventAttribute::Timestamp))
-            }
-            Err(e) => panic!("Failed to parse expression: {:?}", e),
-        }
+        parse_and_compare(
+            "event.timestamp",
+            Expression::EventAttribute(EventAttribute::Timestamp),
+        );
     }
 
     #[test]
     fn test_parse_event_properties() {
-        let input = "event.properties.blah";
-        match ExpressionParser::parse_expression(input) {
-            Ok(expr) => {
-                assert_eq!(
-                    expr,
-                    Expression::EventAttribute(EventAttribute::Properties("blah".to_owned()))
-                )
-            }
-            Err(e) => panic!("Failed to parse expression: {:?}", e),
-        }
+        parse_and_compare(
+            "event.properties.blah",
+            Expression::EventAttribute(EventAttribute::Properties("blah".to_owned())),
+        );
     }
 
     #[test]
     fn test_parse_string() {
-        let input = "'test'";
-        match ExpressionParser::parse_expression(input) {
-            Ok(expr) => {
-                assert_eq!(expr, Expression::String("test".to_owned()))
-            }
-            Err(e) => panic!("Failed to parse expression: {:?}", e),
-        }
+        parse_and_compare("'test'", Expression::String("test".to_owned()));
     }
 
     #[test]
     fn test_parse_concat() {
-        let input = "concat('a', 'b')";
-        match ExpressionParser::parse_expression(input) {
-            Ok(expr) => {
-                assert_eq!(
-                    expr,
-                    Expression::Function(Function::Concat(vec![
-                        Expression::String("a".to_owned()),
-                        Expression::String("b".to_owned()),
-                    ]))
-                )
-            }
-            Err(e) => panic!("Failed to parse expression: {:?}", e),
-        }
+        parse_and_compare(
+            "concat('a', 'b')",
+            Expression::Function(Function::Concat(vec![
+                Expression::String("a".to_owned()),
+                Expression::String("b".to_owned()),
+            ])),
+        );
     }
 
     #[test]
     fn test_parse_ceil() {
-        let input = "ceil(123)";
-        match ExpressionParser::parse_expression(input) {
-            Ok(expr) => {
-                assert_eq!(
-                    expr,
-                    Expression::Function(Function::Ceil(Box::new(Expression::Decimal(123.into()))))
-                )
-            }
-            Err(e) => panic!("Failed to parse expression: {:?}", e),
-        }
+        parse_and_compare(
+            "ceil(123)",
+            Expression::Function(Function::Ceil(Box::new(Expression::Decimal(123.into())))),
+        );
+    }
+
+    #[test]
+    fn test_parse_round() {
+        parse_and_compare(
+            "round(123, 1)",
+            Expression::Function(Function::Round(
+                Box::new(Expression::Decimal(123.into())),
+                Some(Box::new(Expression::Decimal(1.into()))),
+            )),
+        );
+    }
+    #[test]
+    fn test_parse_round_one_arg() {
+        parse_and_compare(
+            "round(123)",
+            Expression::Function(Function::Round(
+                Box::new(Expression::Decimal(123.into())),
+                None,
+            )),
+        );
     }
 }
