a83e747776ef8c33ddbc4d8b580330c36b8aa999
Toon Willems <m@toonwillems.be>
2024-10-14 17:41:03 +0200

    add benchmark in tmp.rb

--- expression-ruby/tmp.rb
+++ expression-ruby/tmp.rb
@@ -1,14 +1,19 @@
 require_relative 'lib/lago-expression'
-
-parsed = ExpressionParser.parse("1 + 2")
-event = Event.new("code", 1234, {"foo" => "bar"})
-
-evaluated = parsed.evaluate(event)
-puts "#{evaluated} is a #{evaluated.class}"
-
+require 'benchmark/ips'
 
 parsed = ExpressionParser.parse("concat(event.properties.foo, '-', event.code)")
 event = Event.new("code", 1234, {"foo" => "bar"})
 
 evaluated = parsed.evaluate(event)
 puts "#{evaluated} is a #{evaluated.class}"
+
+hash = {"foo" => "bar"}
+code = "code"
+
+Benchmark.ips do |x|
+  x.report("regular") { [hash['foo'], '-', code].join('') }
+
+  x.report("rust") { parsed.evaluate(event) }
+  # Compare the iterations per second of the various reports!
+  x.compare!
+end
