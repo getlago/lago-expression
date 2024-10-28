3689e6ef8c0823fcd475907e0de2fc5a57eea0a8
Toon Willems <m@toonwillems.be>
2024-10-28 15:20:39 +0100

    Add wasm_bindgen magic, basic evaluate implementation (no event yet)

--- expression-js/index.js
+++ expression-js/index.js
@@ -1,13 +1,16 @@
-import { parseExpression } from "./pkg";
+import { parseExpression, evaluateExpression } from "./pkg";
 
 var elem = document.getElementById("expression");
 var output = document.getElementById("expression-output");
 var error = document.getElementById("expression-error");
 
+var expression = null;
+
 elem.oninput = function () {
   error.innerHTML = "";
   try {
-    output.innerHTML = parseExpression(elem.value);
+    expression = parseExpression(elem.value);
+    output.innerHTML = evaluateExpression(expression, {});
   } catch (e) {
     error.innerHTML = e;
   }
--- expression-js/src/lib.rs
+++ expression-js/src/lib.rs
@@ -1,4 +1,5 @@
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 
 use expression_core::ExpressionParser;
 extern crate console_error_panic_hook;
@@ -8,9 +9,29 @@ fn start() {
     console_error_panic_hook::set_once();
 }
 
+#[wasm_bindgen]
+#[derive(Debug)]
+pub struct Expression(expression_core::Expression);
+
 #[wasm_bindgen(js_name = parseExpression)]
-pub fn parse_expression(expression: String) -> Result<String, String> {
+pub fn parse_expression(expression: String) -> Result<Expression, String> {
     ExpressionParser::parse_expression(&expression)
         .map_err(|e| format!("{}", e))
-        .map(|e| format!("{e:?}"))
+        .map(Expression)
+}
+
+#[wasm_bindgen(js_name = evaluateExpression)]
+pub fn evaluate_expression(expression: Expression, _event: &JsValue) -> Result<String, String> {
+    console::log_1(&format!("{:?}", expression.0).into());
+    let event = expression_core::Event {
+        ..Default::default()
+    };
+    expression
+        .0
+        .evaluate(&event)
+        .map(|value| match value {
+            expression_core::ExpressionValue::Number(d) => d.to_string(),
+            expression_core::ExpressionValue::String(s) => s,
+        })
+        .map_err(|e| format!("{}", e))
 }
