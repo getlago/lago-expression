28fa277efa91e10c82d7e4e353e22a520ea1c907
Toon Willems <m@toonwillems.be>
2024-11-05 13:30:02 +0100

    Add tests for deserializing behaviour

--- expression-core/src/event.rs
+++ expression-core/src/event.rs
@@ -33,3 +33,77 @@ impl From<u64> for PropertyValue {
         PropertyValue::Number(value.into())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use super::*;
+
+    #[test]
+    fn test_deserialize_with_numbers() {
+        let json = json!({
+            "code": "testing",
+            "timestamp": 123124123123i64,
+            "properties": {
+                "text": "text_value",
+                "number": 300
+            }
+        });
+
+        let event = serde_json::from_value::<Event>(json).expect("expected json to parse");
+
+        let code = "testing".to_owned();
+        let timestamp = 123124123123;
+        let mut properties = HashMap::default();
+        properties.insert("number".to_owned(), PropertyValue::Number(300.into()));
+        properties.insert(
+            "text".to_owned(),
+            PropertyValue::String("text_value".into()),
+        );
+
+        assert_eq!(
+            event,
+            Event {
+                code,
+                timestamp,
+                properties
+            }
+        )
+    }
+
+    #[test]
+    fn test_deserialize_with_floating_point() {
+        let json = r#"{
+            "code": "testing",
+            "timestamp": 123124123123,
+            "properties": {
+                "text": "text_value",
+                "number": 3.2
+            }
+        }"#;
+
+        let event = serde_json::from_str::<Event>(json).expect("expected json to parse");
+
+        let code = "testing".to_owned();
+        let timestamp = 123124123123;
+        let mut properties = HashMap::default();
+        properties.insert(
+            "number".to_owned(),
+            PropertyValue::Number("3.2".parse().unwrap()),
+        );
+        properties.insert(
+            "text".to_owned(),
+            PropertyValue::String("text_value".into()),
+        );
+
+        assert_eq!(
+            event,
+            Event {
+                code,
+                timestamp,
+                properties
+            }
+        )
+    }
+}
